49d250fef337cf6caf57b4600e390cacd45d64fa
AlexisPiramide <220240@fppiramide.com>
2025-05-20 08:50:33 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  chunkSizeWarningLimit: 800,
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
 })
