41741a664ff759c124a0f0272582f881e21af403
AlexisPiramide <220240@fppiramide.com>
2025-05-23 12:44:16 +0200

    -

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,33 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest: {
-        name: 'Your App Name',
-        short_name: 'App',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#317EFB',
-        icons: [
+        "theme_color": "#003da8",
+        "background_color": "#003da8",
+        "icons": [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            "purpose": "maskable",
+            "sizes": "512x512",
+            "src": "icon512_maskable.png",
+            "type": "image/png"
           },
           {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
+            "purpose": "any",
+            "sizes": "512x512",
+            "src": "icon512_rounded.png",
+            "type": "image/png"
+          }
         ],
-      },
-      registerType: 'autoUpdate',
-    }),
+        "orientation": "any",
+        "display": "standalone",
+        "dir": "auto",
+        "lang": "es-ES",
+        "name": "El camino del Paquete",
+        "short_name": "Paqueteria Camino",
+        "start_url": "/",
+        "scope": "/",
+        "id": "/",
+        "description": "Proyecto DAW II Alexis Torres"
+      }),
   ],
   build: {
     chunkSizeWarningLimit: 1600,
