6c7852fbb06cfb9fd8df97ba1cb199f227e675b9
AlexisPiramide <220240@fppiramide.com>
2025-05-23 13:24:49 +0200

    Vuelta a antes de intentar PAW

--- src/main.jsx
+++ src/main.jsx
@@ -49,15 +49,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		<RouterProvider router={router} />
 	</React.StrictMode>
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('✅ Service Worker registrado:', registration);
-      })
-      .catch(error => {
-        console.error('❌ Falló registro SW:', error);
-      });
-  });
-}
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      "name": "El camino del Paquete",
-      "short_name": "Paqueteria",
-      "start_url": "/",
-      "scope": "/",
-      "display": "standalone",
-      "background_color": "#003da8",
-      "theme_color": "#003da8",
-      "icons": [
-        {
-          "src": "/icons/icon512_maskable.png",
-          "type": "image/png",
-          "sizes": "512x512",
-          "purpose": "maskable"
-        },
-        {
-          "src": "/icons/icon512_rounded.png",
-          "type": "image/png",
-          "sizes": "512x512",
-          "purpose": "any"
-        }
-      ]
-    }
-    ),
-  ],
+  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
-  },
+    chunkSizeWarningLimit: 1600
+  }
 })
